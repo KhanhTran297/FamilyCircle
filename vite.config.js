f441a69e90523d71df8b4932aa2f031cf91046b6
KhanhTran297 <giakhanh2972002@gmail.com>
2023-09-14 23:23:54 +0700

    chore: setup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   plugins: [react()],
-})
+  resolve: {
+    // alias: {
+    //   "@": path.resolve(__dirname, "./src"),
+    // },
+  },
+});
