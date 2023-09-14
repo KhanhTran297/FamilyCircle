e28fa0f2a42492098c293fbee91a2cb3f1ee3965
KhanhTran297 <giakhanh2972002@gmail.com>
2023-09-14 23:25:04 +0700

    feat: add account api method

---
+++ src/api/account.js
@@ -0,0 +1,45 @@
+import UseCallApi from "../hooks/UseCallApi";
+
+const { UseGet, UsePost, UseEdit } = UseCallApi();
+export const authLoginApi = (params) => {
+  const url = "/api/token";
+  return UsePost({ url, params });
+};
+export const getAccountProfileApi = () => {
+  const url = "/v1/user/profile";
+  return UseGet({ url, requiredToken: true });
+};
+export const SignUpApi = (params) => {
+  const url = "/v1/user/register";
+  return UsePost({ url, params });
+};
+//logout
+export const authLogoutApi = () => {
+  const url = "/account/logout";
+  return UseGet({ url, requiredToken: true });
+};
+//edit
+export const editProfileApi = (params) => {
+  const url = "/v1/user/update";
+  return UseEdit({ url, requiredToken: true, params });
+};
+//changePassword
+export const changePasswordApi = (params) => {
+  const url = "/v1/account/change-password";
+  return UseEdit({ url, requiredToken: true, params });
+};
+//sendOtp
+export const sentOtpApi = (params) => {
+  const url = "/v1/user/send-otp-code";
+  return UseEdit({ url, params });
+};
+//checkOtp
+export const checkOtpApi = (params) => {
+  const url = "/v1/user/check-otp-code";
+  return UseEdit({ url, params });
+};
+//createNewPass
+export const createNewPasswordApi = (params) => {
+  const url = "/v1/user/change-password";
+  return UseEdit({ url, params });
+};
