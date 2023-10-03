c8c3a1a1687d2c42ddcb63caff693c2d6b35ab02
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-03 09:53:10 +0700

    feat: add expertContent

---
+++ src/components/admin/ExpertsContent.jsx
@@ -0,0 +1,16 @@
+const ExpertsContent = (props) => {
+  const { bg } = props;
+  return (
+    <div
+      style={{
+        padding: 24,
+        minHeight: 360,
+        background: bg,
+      }}
+    >
+      ExpertsContent
+    </div>
+  );
+};
+
+export default ExpertsContent;
