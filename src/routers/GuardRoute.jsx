f359dc36686f14154f196346e26822b3a9f304fd
Sirenef <anhngocbui332@gmail.com>
2023-10-04 00:48:38 +0700

    feat: fix layout home

--- src/routers/GuardRoute.jsx
+++ src/routers/GuardRoute.jsx
@@ -1,42 +1,24 @@
-// import { useEffect } from "react";
-// import UseCookie from "../hooks/UseCookie";
-// import useAccount from "../hooks/useAccount";
-// import { useNavigate } from "react-router-dom";
-
-// const GuardRoute = ({ children }) => {
-//   const { isLoggedIn } = UseCookie();
-//   const { profileAccount, getProfileAccount, loadingPage } = useAccount();
-//   const navigate = useNavigate();
-//   useEffect(() => {
-//     if (isLoggedIn()) {
-//       if (!profileAccount?.data) {
-//         getProfileAccount();
-//         navigate("/index");
-//       } else {
-//         navigate("/index");
-//         return;
-//       }
-//     } else {
-//       navigate("/");
-//     }
-//   }, []);
-//   return <div>{loadingPage ? <div>Loading</div> : <div>{children}</div>}</div>;
-// };
-
-// export default GuardRoute;
 import { useEffect } from "react";
-// import UseCookie from "../hooks/UseCookie";
+import UseCookie from "../hooks/UseCookie";
 import useAccount from "../hooks/useAccount";
 import { useNavigate } from "react-router-dom";
 
 const GuardRoute = ({ children }) => {
-  // const { isLoggedIn } = UseCookie();
-  const { loadingPage } = useAccount();
+  const { isLoggedIn } = UseCookie();
+  const { profileAccount, getProfileAccount, loadingPage } = useAccount();
   const navigate = useNavigate();
   useEffect(() => {
-    navigate("/index");
+    if (isLoggedIn()) {
+      if (!profileAccount?.data) {
+        getProfileAccount();
+        navigate("/index");
+      } else {
+        navigate("/index");
+        return;
+      }
+    } else {
+      navigate("/");
+    }
   }, []);
   return <div>{loadingPage ? <div>Loading</div> : <div>{children}</div>}</div>;
 };
-
-export default GuardRoute;
