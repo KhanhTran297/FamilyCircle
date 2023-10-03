dd184be64111bd6f79c7d9c07cdf295c3f34abd2
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-03 09:55:14 +0700

    fix: add refreshtoken

--- src/hooks/UseCookie.js
+++ src/hooks/UseCookie.js
@@ -1,75 +1,74 @@
 import Cookies from "js-cookie";
 
 function UseCookie() {
-  // const accessTokenKey = import.meta.env.VITE_ACCESS_TOKEN;
-  //   const refreshTokenKey = import.meta.env.VITE_REFRESH_TOKEN;
+  // const "accessToken" = import.meta.env.VITE_ACCESS_TOKEN;
+  //   const "refreshToken" = import.meta.env.VITE_REFRESH_TOKEN;
   const objCookies = {
     expires: 30,
     secure: true,
     path: "/",
     // domain: process.env.COOKIE_DOMAIN,
   };
-  const saveToken = (access_token) => {
-    Cookies.set("accessToken", access_token, {
-      ...objCookies,
-    });
+  // const saveToken = (access_token) => {
+  //   Cookies.set("accessToken", access_token, {
+  //     ...objCookies,
+  //   });
+  // };
+
+  // const getToken = () => {
+  //   const access_token = Cookies.get("accessToken");
+  //   return {
+  //     access_token,
+  //   };
+  // };
+  // const removeToken = () => {
+  //   Cookies.remove("accessToken", {
+  //     ...objCookies,
+  //   });
+  // };
+  // const isLoggedIn = () => !!getToken().access_token;
+
+  //------------------------- Co refreshToken--------------------
+  const saveToken = (access_token, refresh_token) => {
+    if (access_token && refresh_token) {
+      Cookies.set("accessToken", access_token, {
+        ...objCookies,
+      });
+      Cookies.set("refreshToken", refresh_token, {
+        ...objCookies,
+      });
+    } else {
+      Cookies.remove("accessToken", {
+        ...objCookies,
+        // domain: process.env.COOKIE_DOMAIN,
+      });
+      Cookies.remove("refreshToken", {
+        ...objCookies,
+        // domain: process.env.COOKIE_DOMAIN,
+      });
+    }
   };
 
   const getToken = () => {
     const access_token = Cookies.get("accessToken");
+    const refresh_token = Cookies.get("refreshToken");
     return {
       access_token,
+      refresh_token,
     };
   };
   const removeToken = () => {
-    Cookies.remove("accessToken", {
-      ...objCookies,
-    });
+    const access_token = Cookies.get("accessToken");
+    if (access_token) {
+      Cookies.remove("accessToken", {
+        ...objCookies,
+      });
+      Cookies.remove("refreshToken", {
+        ...objCookies,
+      });
+    }
   };
   const isLoggedIn = () => !!getToken().access_token;
-
-  //------------------------- Co refreshToken--------------------
-  //   const saveToken = (access_token, refresh_token) => {
-  //     if (access_token && refresh_token) {
-  //       Cookies.set(accessTokenKey, access_token, {
-  //         ...objCookies,
-  //       });
-  //       Cookies.set(refreshTokenKey, refresh_token, {
-  //         ...objCookies,
-  //       });
-  //     } else {
-  //       Cookies.remove(accessTokenKey, {
-  //         ...objCookies,
-  //         path: "/",
-  //         // domain: process.env.COOKIE_DOMAIN,
-  //       });
-  //       Cookies.remove(refreshTokenKey, {
-  //         ...objCookies,
-  //         path: "/",
-  //         // domain: process.env.COOKIE_DOMAIN,
-  //       });
-  //     }
-  //   };
-
-  //   const getToken = () => {
-  //     const access_token = Cookies.get(accessTokenKey);
-  //     const refresh_token = Cookies.get(refreshTokenKey);
-  //     return {
-  //       access_token,
-  //       refresh_token,
-  //     };
-  //   };
-  //   const logOut = () => {
-  //     const access_token = Cookies.get(accessTokenKey);
-  //     if (access_token) {
-  //       Cookies.remove(accessTokenKey, {
-  //         ...objCookies,
-  //       });
-  //       Cookies.remove(refreshTokenKey, {
-  //         ...objCookies,
-  //       });
-  //     }
-  //   };
   return {
     isLoggedIn,
     saveToken,
