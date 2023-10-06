dd033c1a11b42b39bb05980a979222c1f3e37cf9
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-06 09:34:09 +0700

    refactor: refactor import react-query

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
-import { QueryClient, QueryClientProvider } from "react-query";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <RecoilRoot>
         <App />
       </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </Provider>
   // <React.StrictMode>
