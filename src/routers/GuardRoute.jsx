9b896452c5d12c1cf796469794aca672768c847a
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-06 09:39:27 +0700

    fix: remove spin, comment loadingPage

--- src/routers/GuardRoute.jsx
+++ src/routers/GuardRoute.jsx
@@ -3,7 +3,6 @@ import UseCookie from "../hooks/UseCookie";
 import useAccount from "../hooks/useAccount";
 import { useNavigate } from "react-router-dom";
 import LoadingPage from "../components/shared/LoadingPage";
-import { Spin } from "antd";
 
 const GuardRoute = ({ children }) => {
   const { isLoggedIn } = UseCookie();
@@ -24,15 +23,15 @@ const GuardRoute = ({ children }) => {
   }, []);
   return (
     <div>
-      {loadingPage ? (
+      {/* {loadingPage ? (
         <LoadingPage
           css={
             "absolute  justify-center items-center place-items-center h-full w-full bg-white dark:bg-black"
           }
         />
-      ) : (
-        <div>{children}</div>
-      )}
+      ) : ( */}
+      <div>{children}</div>
+      {/* )} */}
     </div>
   );
 };
