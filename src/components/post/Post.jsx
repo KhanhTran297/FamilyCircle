1a17c48af6a10f0dc8fdd3cb20c7d9315c3b73e0
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-15 13:55:48 +0700

    fix: delete type post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,8 +7,7 @@ import { ILocalViewMore } from "../svg/viewmore";
 import FooterPost from "./FooterPost";
 import HeaderPost from "./HeaderPost";
 import DOMPurify from "dompurify";
-import { Avatar, Input } from "antd";
-import { ILocalArrowRight } from "../svg/arrow_right";
+
 // import { ILocalDot } from "../svg/Dot";
 // import { ILocalMore } from "../svg/more";
 
@@ -18,17 +17,12 @@ const Post = (props) => {
   const navigate = useNavigate();
   let limit = 1000;
   let content;
-  const cssPost =
-    props.type === "basic"
-      ? "flex flex-col max-h-[320px] w-full  overflow-hidden relative "
-      : "flex flex-col h-max w-full  overflow-hidden relative ";
-  if (props.content.length > limit && props.type === "basic") {
+  if (props.content.length > limit) {
     content = props.content.slice(0, limit) + "...";
   } else {
     content = props.content;
   }
   const sanitizedContent = DOMPurify.sanitize(content);
-
   return (
     <div className="flex flex-col items-start xl:gap-6 gap-6 p-6 pt-3  rounded-[24px] w-full  bg-[#FFF8F8] cursor-pointer">
       <div className="flex flex-row items-start self-stretch gap-2">
@@ -38,30 +32,21 @@ const Post = (props) => {
         <HeaderPost {...props} />
       </div>
       <div className="flex flex-col w-full gap-6">
-        <div className={cssPost}>
+        <div className="flex flex-col max-h-[320px] w-full  overflow-hidden relative ">
           <div className="flex flex-col items-start w-full gap-6 shrink-0">
-            {props.type == "basic" ? (
-              <div
-                className="w-full h-auto text-sm font-normal read-more-read-less font-roboto"
-                dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-                onClick={() =>
-                  props.type === "basic" && navigate(`/post/${props.id}`)
-                }
-              ></div>
-            ) : (
-              <div
-                className="w-full h-max text-sm font-normal read-more-read-less font-roboto"
-                dangerouslySetInnerHTML={{ __html: props.content }}
-              ></div>
-            )}
+            <div
+              className="w-full h-auto text-sm font-normal read-more-read-less font-roboto"
+              dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+              onClick={() => navigate(`/post/${props.id}`)}
+            ></div>
           </div>
-          {props.content.length > limit && props.type == "basic" ? (
+          {props.content.length > limit ? (
             <div className="w-full h-[72px] absolute bottom-0 bg-post "></div>
           ) : (
             ""
           )}
         </div>
-        {props.content.length > limit && props.type === "basic" ? (
+        {props.content.length > limit ? (
           <div className="flex flex-col items-center justify-center self-stretch gap-[10px] cursor-pointer">
             <button className="cursor-pointer flex h-10 gap-[7px] bg-[#A73574] rounded-[36px] px-4 flex-row items-center">
               <ILocalViewMore fill="#FFF8F8" />
@@ -74,43 +59,8 @@ const Post = (props) => {
           ""
         )}
       </div>
-      {props.type === "detail" && (
-        <div className="border-t-[1px] border-b-[1px] border-[#F1DEE4] h-[1px] w-full"></div>
-      )}
 
       <FooterPost />
-      {props.type === "detail" && (
-        <div className="border-t-[1px] border-b-[1px] border-[#F1DEE4] h-[1px] w-full"></div>
-      )}
-      <div className=" w-full flex flex-col gap-6">
-        <div className="box-comment">
-          <div className="comment">
-            <div className="left"></div>
-            <div className="right">
-              <div className="body">
-                <div className="title">
-                  <div className="name"></div>
-                  <div className="kind"></div>
-                  <div className="date"></div>
-                </div>
-                <div className="content"></div>
-              </div>
-              <div className="footer"></div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex flex-row gap-2">
-          <Avatar
-            src="https://s3.ap-southeast-1.amazonaws.com/family.circle/avatar/AVATAR_tB5idnWvVj.jpg"
-            className=" w-10 h-10 rounded-[40px]"
-          />
-          <Input
-            placeholder="Say some thing about this post..."
-            className=" flex flex-row-reverse pl-4 gap-2 h-10 flex-1-0-0 rounded-[100px] "
-            prefix={<ILocalArrowRight fill="#1F1A1C" />}
-          ></Input>
-        </div>
-      </div>
     </div>
   );
 };
@@ -121,7 +71,6 @@ Post.propTypes = {
   createdAt: PropTypes.string.isRequired,
   likes: PropTypes.number.isRequired,
   comments: PropTypes.number.isRequired,
-  type: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 export default Post;
