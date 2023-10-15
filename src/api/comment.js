bd0130745896c376c45965d140be53962669cca7
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-15 20:29:45 +0700

    feat: add getListcomment and childcomment api

--- src/api/comment.js
+++ src/api/comment.js
@@ -2,8 +2,11 @@ import UseCallApi from "../hooks/UseCallApi";
 
 const { UseGet, UsePost, UseEdit, UseDelete } = UseCallApi();
 
-export const getListCommentApi = (postId, commentId) => {
-  console.log("commentId:", commentId);
+export const getListCommentApi = (postId, commentId, size, pageParam = 0) => {
+  const url = `v1/comment/list?postId=${postId}&parentId=${commentId}&page=${pageParam}&size=${size}`;
+  return UseGet({ url, requiredToken: true });
+};
+export const getListChildCommentApi = (postId, commentId) => {
   const url = `v1/comment/list?postId=${postId}&parentId=${commentId}`;
   return UseGet({ url, requiredToken: true });
 };
