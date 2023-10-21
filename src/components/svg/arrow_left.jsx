281edc9b60fcb0d46f0adf9f8b96461a9d01ab8d
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-21 17:52:00 +0700

    feat: add icon svg

--- src/components/svg/arrow_left.jsx
+++ src/components/svg/arrow_left.jsx
@@ -1,32 +1,36 @@
 import PropTypes from "prop-types";
 
-export const ILocalArrowLeft = ({ fill }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-  >
-    <mask
-      id="mask0_734_5257"
-      style={{ maskType: "alpha" }}
-      maskUnits="userSpaceOnUse"
-      x="0"
-      y="0"
+export const ILocalArrowLeft = ({ fill, className, eventClick }) => (
+  <div className={className} onClick={eventClick}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
+      viewBox="0 0 24 24"
+      fill="none"
     >
-      <rect width="24" height="24" fill="#D9D9D9" />
-    </mask>
-    <g mask="url(#mask0_734_5257)">
-      <path
-        d="M12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825L13.425 18.6L12 20Z"
-        fill={fill}
-      />
-    </g>
-  </svg>
+      <mask
+        id="mask0_734_5257"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="24"
+        height="24"
+      >
+        <rect width="24" height="24" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_734_5257)">
+        <path
+          d="M12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825L13.425 18.6L12 20Z"
+          fill={fill}
+        />
+      </g>
+    </svg>
+  </div>
 );
 ILocalArrowLeft.propTypes = {
   fill: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  eventClick: PropTypes.func,
 };
