b78f31975796b62c3ea79353102041c539478ae4
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-27 15:38:30 +0700

    feat: add logic react show comment and reply

--- src/components/comment/CommentFooter.jsx
+++ src/components/comment/CommentFooter.jsx
@@ -2,29 +2,82 @@ import { ILocalComment } from "../svg/comment";
 import { ILocalHeartComment } from "../svg/hear_comment";
 import { ILocalReply } from "../svg/reply";
 import IconFooter from "./IconFooter";
-
-const CommentFooter = () => {
-  const handleClick = () => {
-    console.log("reply");
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+import useAccount from "../../hooks/useAccount";
+import { ILocalEmptyHeart } from "../svg/empty_heart";
+import useComment from "../../hooks/useComment";
+const CommentFooter = ({
+  eventReply,
+  data,
+  countComment,
+  eventShowReply,
+  root,
+}) => {
+  const { profileAccount, profileExpertAccount } = useAccount();
+  const { reactcomment } = useComment(data.id, false);
+  const handleReactComment = () => {
+    reactcomment({ commentId: data.id, kind: 1 });
   };
+
+  const checkReact = () => {
+    if (profileAccount) {
+      const check = data.commentReactions?.filter(
+        (item) => item.id === profileAccount.data.id
+      );
+
+      if (check) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      const check = data.commentReactions?.filter(
+        (item) => item.id === profileExpertAccount.data.id
+      );
+      if (check) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+  useEffect(() => {
+    checkReact();
+  }, []);
   return (
-    <div className=" xl:flex xl:flex-row xl:gap-4 mt-1">
-      <IconFooter count={1} check={true}>
-        <ILocalHeartComment fill="#A73574" className="p-[10px]" />
+    <div className=" xl:flex xl:flex-row xl:gap-4 mt-1 pb-4">
+      <IconFooter
+        check={true}
+        count={data.commentReactions?.length || 0}
+        handleClick={() => handleReactComment()}
+      >
+        {checkReact() ? (
+          <ILocalHeartComment fill="#A73574" className="" />
+        ) : (
+          <ILocalEmptyHeart className="" />
+        )}
       </IconFooter>
-      <IconFooter count={0} check={true}>
-        <ILocalComment
-          fill="#A73574"
-          width={24}
-          height={24}
-          className="p-[10px]"
-        />
-      </IconFooter>
-      <IconFooter check={false} handleClick={handleClick}>
-        <ILocalReply fill="#1F1A1C" className="p-[10px]" />
+      {root && (
+        <IconFooter
+          count={countComment || 0}
+          check={true}
+          handleClick={() => eventShowReply()}
+        >
+          <ILocalComment fill="#A73574" width={24} height={24} className="" />
+        </IconFooter>
+      )}
+      <IconFooter check={false} handleClick={() => eventReply()}>
+        <ILocalReply fill="#1F1A1C" className="" />
       </IconFooter>
     </div>
   );
 };
-
+CommentFooter.propTypes = {
+  eventReply: PropTypes.func.isRequired,
+  eventShowReply: PropTypes.func,
+  data: PropTypes.object.isRequired,
+  countComment: PropTypes.number,
+  root: PropTypes.bool.isRequired,
+};
 export default CommentFooter;
