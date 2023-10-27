cc707b679237fdfa73a2bacdbb757bab7ad8b2b8
KhanhTran297 <giakhanh2972002@gmail.com>
2023-10-27 15:38:58 +0700

    style: css

--- src/components/comment/CommentLeft.jsx
+++ src/components/comment/CommentLeft.jsx
@@ -2,21 +2,31 @@ import { Avatar } from "antd";
 
 import PropTypes from "prop-types";
 
-const CommentLeft = ({ img }) => {
+const CommentLeft = ({ img, root, showReply, activeCol }) => {
   return (
-    <div className="flex justify-center">
+    <div
+      className={`flex justify-center  ${
+        ((root && showReply) || activeCol) && "items-center flex-col gap-2"
+      } `}
+    >
       <Avatar
         src={
           img ||
           "https://s3.ap-southeast-1.amazonaws.com/family.circle/avatar/AVATAR_tB5idnWvVj.jpg"
         }
-        className=" w-10 h-10 rounded-[40px]"
+        className=" w-10 h-10 rounded-[40px] "
       />
+      {((root && showReply) || activeCol) && (
+        <div className=" w-[1px] flex-1 bg-[#F1DEE4]"></div>
+      )}
     </div>
   );
 };
 
 CommentLeft.propTypes = {
   img: PropTypes.string.isRequired,
+  root: PropTypes.bool.isRequired,
+  showReply: PropTypes.bool,
+  activeCol: PropTypes.bool,
 };
 export default CommentLeft;
