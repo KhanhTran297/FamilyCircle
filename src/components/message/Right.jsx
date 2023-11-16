e8ab0642e449facf6c3a23c3d9a9b908a0861a14
KhanhTran297 <giakhanh2972002@gmail.com>
2023-11-16 23:55:21 +0700

    feat: add message layout

---
+++ src/components/message/Right.jsx
@@ -0,0 +1,56 @@
+import { Input } from "antd";
+
+const Right = () => {
+  return (
+    <div className=" flex flex-col items-start flex-[1_1_0] self-stretch border-r border-solid border-r-[#F1DEE4]">
+      <div className=" flex pt-3 pb-3 pl-4 pr-4 items-center self-stretch gap-2">
+        <div className=" w-10 h-10 rounded-[20px]">
+          <img
+            src="https://i.pinimg.com/236x/18/28/2f/18282f404ea132c3423e4ac178259b7b.jpg"
+            alt=""
+            className=" w-full h-full rounded-[20px]"
+          />
+        </div>
+        <p className=" text-light_surface_on_surface font-roboto text-sm font-medium">
+          Brooklyn Simmons
+        </p>
+      </div>
+      <div className=" flex p-6 flex-col items-start gap-3 self-stretch h-full"></div>
+      <div className=" h-16 flex pl-6 pr-6 items-center gap-[10px] self-stretch border-t border-solid border-t-[#f1DEE4]">
+        <input
+          placeholder="Text your message..."
+          className=" border-none outline-none flex-[1_0_0] text-[#504348] font-roboto text-sm font-normal "
+        ></input>
+        <div className="flex w-10 h-10 p-[10px] flex-col justify-center items-center gap-[10px] rounded-[20px] hover:bg-bgButtonHover cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <mask
+              id="mask0_1169_1014"
+              style={{ maskType: "alpha" }}
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+            >
+              <rect width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_1169_1014)">
+              <path
+                d="M3 20V4L22 12L3 20ZM5 17L16.85 12L5 7V10.5L11 12L5 13.5V17Z"
+                fill="#1F1F1F"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Right;
