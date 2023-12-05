14a48e88cb091d78d82937fa111550a5d11bd5d4
KhanhTran297 <giakhanh2972002@gmail.com>
2023-12-06 00:06:00 +0700

    fix: fix bug can't react post

--- src/components/userprofile/TabPosts.jsx
+++ src/components/userprofile/TabPosts.jsx
@@ -35,21 +35,25 @@ const TabPosts = () => {
               {Array.isArray(page.data.content) && // Kiểm tra xem page.data là mảng
                 page.data.content
                   // .filter((post) => post.kind === 1)
-                  .map((post) => (
-                    // console.log(post.id),
-                    <Post
-                      key={post.id}
-                      id={post.id}
-                      content={post.content}
-                      fullname={post.owner.fullName}
-                      kind={post.owner.kind}
-                      modifiedDate={post.modifiedDate}
-                      createdDate={post.createdDate}
-                      idowner={post.owner.id}
-                      kindPost={post.kind}
-                      avatar={post.owner.avatar}
-                    />
-                  ))}
+                  .map(
+                    (post, index) => (
+                      console.log(post.kind),
+                      (
+                        <Post
+                          key={index}
+                          id={post.id}
+                          content={post.content}
+                          fullname={post.owner.fullName}
+                          kind={post.owner.kind}
+                          modifiedDate={post.modifiedDate}
+                          createdDate={post.createdDate}
+                          idowner={post.owner.id}
+                          kindPost={post.kind}
+                          avatar={post.owner.avatar}
+                        />
+                      )
+                    )
+                  )}
             </div>
           </div>
         ))}
