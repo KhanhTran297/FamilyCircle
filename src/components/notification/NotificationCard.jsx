a40794b0b88d70fdc4d64d4e40f1e73fc7663209
Sirenef <anhngocbui332@gmail.com>
2023-12-07 13:29:51 +0700

    fix navigate

--- src/components/notification/NotificationCard.jsx
+++ src/components/notification/NotificationCard.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import AvtUser from "../shared/AvtUser";
-import { useNavigate } from "react-router-dom";
 
 const NotificationCard = (props) => {
-  const navigate = useNavigate();
-
   return (
-    <div
-      className="flex flex-row items-center self-stretch gap-2 px-6 py-3 border-b-[1px] border-solid border-[#F1DEE4]"
-      onClick={() => {
-        navigate(`/profile/${props.id}`);
-      }}
-    >
+    <div className="flex flex-row items-center self-stretch gap-2 px-6 py-3 border-b-[1px] border-solid border-[#F1DEE4]">
       <AvtUser imageUrl={props.avatar} />
       <div className="flex flex-col items-start self-stretch gap-1">
         <div className="flex flex-row items-start self-stretch gap-1 ">
