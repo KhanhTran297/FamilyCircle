297fcc2748c81dad4938ba5d3d04a13c3f1f5391
KhanhTran297 <giakhanh2972002@gmail.com>
2024-06-13 14:56:03 +0700

    fix: firebase-messaging-service

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,41 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+// import { initializeApp } from "firebase/app";
+// import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDokpaz7n7nOteLkV7X-4_oVNBHRk20tm4",
+//   authDomain: "familycircle-e948c.firebaseapp.com",
+//   projectId: "familycircle-e948c",
+//   storageBucket: "familycircle-e948c.appspot.com",
+//   messagingSenderId: "888169418055",
+//   appId: "1:888169418055:web:ca575771c33af9aead5062",
+//   measurementId: "G-K1SC03SEJ8",
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+// const messaging = getMessaging(firebaseApp);
+
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: payload.notification.image,
+//   };
+
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
+<script type="module">
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.6.7/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.6.7/firebase-messaging-compat.js"
+);
 
 const firebaseConfig = {
   apiKey: "AIzaSyDokpaz7n7nOteLkV7X-4_oVNBHRk20tm4",
@@ -11,15 +47,15 @@ const firebaseConfig = {
   measurementId: "G-K1SC03SEJ8",
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+firebase.initializeApp(firebaseConfig);
 
-onBackgroundMessage(messaging, (payload) => {
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
@@ -28,3 +64,5 @@ onBackgroundMessage(messaging, (payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+</script>
+
