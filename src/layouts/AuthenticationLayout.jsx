4e9dfbc6749580c592402d5b5546d44f8fb5aa90
KhanhTran297 <giakhanh2972002@gmail.com>
2024-06-18 19:35:34 +0700

    fix: filter CourseId and ban route

--- src/layouts/AuthenticationLayout.jsx
+++ src/layouts/AuthenticationLayout.jsx
@@ -2,14 +2,15 @@ import { Outlet } from "react-router-dom";
 import SwitchDarkMode from "../components/shared/SwitchDarkMode";
 
 const AuthenticationLayout = () => {
+  localStorage.clear();
   return (
-    <div className=" absolute h-screen w-full">
-      {/* <div className=" flex flex-row items-center justify-between p-2  bg-white dark:bg-black ">
-        <div className=" relative w-10 h-10">
+    <div className="absolute w-full h-screen ">
+      {/* <div className="flex flex-row items-center justify-between p-2 bg-white  dark:bg-black">
+        <div className="relative w-10 h-10 ">
           <img
             src="/logo.jpg"
             alt=""
-            className=" absolute w-full h-full rounded-full"
+            className="absolute w-full h-full rounded-full "
           />
         </div>
         <SwitchDarkMode />
--- src/layouts/CmsLayout.jsx
+++ src/layouts/CmsLayout.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 const CmsLayout = () => {
+  const userKind = JSON.parse(localStorage.getItem("userKind"));
+  useEffect(() => {
+    if (userKind) {
+      if (userKind === 2 || userKind === 3) {
+        history.back();
+      }
+    }
+  }, [userKind]);
   return (
-    <div className=" absolute h-full w-full">
+    <div className="absolute w-full h-full ">
       <Outlet />
     </div>
   );
